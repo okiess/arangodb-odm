ffed4b931524e1523228522a0c70bec4441948a1
Oliver Kiessler <kiessler@inceedo.com>
2012-04-07 19:52:05 +0200

    CRUD

--- lib/avocadodb-rb.rb
+++ lib/avocadodb-rb.rb
@@ -3,73 +3,137 @@ require "httparty"
 require "json"
 
 module AvocadoDb
-  class Base
+  class Transport
     include HTTParty
     base_uri 'http://localhost:8529'
-    
+  end
+end
+
+module AvocadoDb
+  class DocumentProxy
+    attr_accessor :collection, :collection_id, :location, :_id, :_rev
+    attr_reader :attributes
+
     def initialize(collection)
-      @attributes = {}; @collection = collection; @is_new = true
+      @attributes = {}; self.collection = collection
+    end
+
+    def is_new?; self._id.nil?; end
+  end
+end
+
+module AvocadoDbProperties
+  def self.included(klass)  
+    klass.extend ClassMethods  
+  end  
+
+  module ClassMethods  
+    def collection(value)
+      (class << self; self; end).send(:define_method, 'collection') do
+        value
+      end
     end
     
-    def collection=(value); @collection = value; end
-    def collection; @collection; end
-    def attributes; @attributes; end
-    def location; @location; end
-    def id; @_id; end
-    def version; @_etag; end
+    def transport(value)
+      (class << self; self; end).send(:define_method, 'transport') do
+        value
+      end
+    end
+    
+    def target(value)
+      (class << self; self; end).send(:define_method, 'target') do
+        value
+      end
+    end
+  end
+end
 
-    def all
-      # TODO
-    end
-    def self.find(collection, id)
-      document = self.new(collection)
-      document.find(id)
-      document
-    end
-    def find(id)
-      raise "missing id" if id.nil?
-      res = Base.get("/_document/#{id}")
-      @attributes.clear
-      res.parsed_response.each {|k, v| self.send("#{k}=".to_sym, v) unless k == "_id" }
-      @_id = res.parsed_response['_id']
-      @_etag = @_id.split(":").last
-      @is_new = false
-      @attributes
-    end
-    def reload; self.find(self.id); end
+# Base class for AvocadoDB documents.
+#
+# Example:
+# class ExampleDocument < AvocadoDb::Base
+#  collection 'examples'
+# end
+#
+# doc = ExampleDocument.new
+# doc.foo = "bar"
+# _id = doc.save
+#
+# doc.foo = "bar2"
+# doc.save
+#
+# doc.destroy
+# 
+# doc = ExampleDocument.find(_id)
+#
+module AvocadoDb
+  class Base
+    include AvocadoDbProperties
+    transport AvocadoDb::Transport
+    target AvocadoDb::DocumentProxy
+    attr_reader :target
 
-    def is_new?; @is_new; end
+    def initialize
+      @transport = self.class.transport
+      @target = self.class.target.new(self.class.collection)
+    end
+
+    def self.find(document_handle)
+      raise "missing document handle" if document_handle.nil?
+      res = transport.get("/document/#{document_handle}")
+      if res.code == 200
+        document = self.new
+        res.parsed_response.each {|k, v| document.send("#{k}=".to_sym, v) unless ['_id', '_rev'].include?(k)}
+        document.target._id = res.parsed_response['_id']
+        document.target._rev = res.parsed_response['_rev']
+        document.target.location = "/document/#{document.target._id}"
+        document
+      else
+        nil
+      end
+    end
+
     def save
-      if is_new?
-        res = Base.post("/_document/#{self.collection}", :body => @attributes.to_json)
-        # puts res.inspect
-        if res.code == 201
-          @location = res.headers["location"]
-          @_id = @location.split("/").last
-          @_etag = @_id.split(":").last
-          @is_new = false
-          return @_id
+      if @target.is_new?
+        res = @transport.post("/document/?collection=#{@target.collection}&createCollection=true", :body => @target.attributes.to_json)
+        if res.code == 201 || res.code == 202
+          @target.location = res.headers["location"]
+          @target._id = res.parsed_response["_id"]
+          @target._rev = res.headers["etag"]
+          return @target._id
         end
       else
-        res = Base.put("/_document/#{self.id}", :body => @attributes.to_json)    
-        @_etag = res.headers['etag']
+        res = @transport.put(@target.location, :body => @target.attributes.to_json)    
+        return (@target._rev = res.parsed_response['_rev'])
       end
+      nil
     end
 
     def destroy
       unless is_new?
-        Base.delete("/_document/#{self.id}")
-        @is_new = true; @attributes.clear; @_id = nil; @_etag = nil; @location = nil
+        res = @transport.delete(self.location)
+        if res.code == 200
+          @target.location = nil
+          @target._id = nil
+          @target._rev = nil
+        end
       end
     end
-
-    private
+    
+    protected
     def method_missing(method, *args, &block)
-      if method.to_s[-1, 1] == '='
-        @attributes[method.to_s.gsub('=', '')] = args.first
+      method = method.to_s
+      if method[-1, 1] == '='
+        @target.attributes[method.gsub('=', '')] = args.first unless ['_id=', '_rev='].include?(method)
       else
-        @attributes[method.to_s]
+        val = @target.attributes[method]
+        val = @target.send(method.to_sym) unless val
+        val
       end
     end
   end
+end
+
+class ExampleDocument < AvocadoDb::Base
+  collection 'examples'
 end
